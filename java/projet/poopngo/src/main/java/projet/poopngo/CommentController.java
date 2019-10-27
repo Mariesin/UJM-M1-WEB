d5d880c5ddef85fb0a7d5171aafbee22f394ea76
faresdabek <faresdabek@gmail.com>
2019-10-27 23:17:41 +0100

    commit3 fares

--- java/projet/poopngo/src/main/java/projet/poopngo/CommentController.java
+++ java/projet/poopngo/src/main/java/projet/poopngo/CommentController.java
@@ -1,14 +1,17 @@
 package projet.poopngo;
 
+//import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class CommentController {
@@ -18,22 +21,39 @@ public class CommentController {
 	@Autowired
 	private PersonRepository personeService;
 	
+	@RestController
+	public class PersonController {
+
+	    @Autowired
+	    PersonService personService;
+
+	    @GetMapping("/persons")
+	    private List<Person> getAllPersons() {
+	        return personService.getAllPersons();
+	        
+	    }
+	}
+	
+	
 	@GetMapping(path = "/comment")
     public String home(){
-		//list of target
-		List<Person> list = (List<Person>) personeService.findAll();
+		//list of person
+		//List<Person> list = (List<Person>) personeService.findAll();
         return "comment";
     }
 //add comment 
 	@PostMapping(path = "/add/comment")
     public String addComment(@RequestParam String text,@RequestParam Long target){
-		//target id : id de persone a commenter
+		//target id : id de persone a commenter liste derou
 		Optional<Person> p = personeService.findById(target);
+		//recuperer la personne 
 		Person t = p.get(); 
 		Comment m= new Comment();
 		m.setTarget(t);
 		m.setText(text);
 		//m.setSource();
+		//m.seTarget();
+
 		service.save(m);
         return "comment";
     }
---
+++ java/projet/poopngo/src/main/java/projet/poopngo/PersonService.java
@@ -0,0 +1,29 @@
+package projet.poopngo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+	public class PersonService {
+
+	    @Autowired
+	    PersonRepository personRepository;
+
+	    public List<Person> getAllPersons() {
+	        List<Person> persons = new ArrayList<Person>();
+	        personRepository.findAll().forEach(person -> persons.add(person));
+	        return persons;
+	    }
+
+	    public Person getPersonById(int id) {
+	        return personRepository.findById((long) id).get();
+	    }
+
+	    public void saveOrUpdate(Person person) {
+	        personRepository.save(person);
+	    }
+
+}
